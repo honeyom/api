8e259ce911f3ffa99bb52e59894a4e8141412ac3
owen-001 <dd536819@gmail.com>
2019-12-05 23:18:54 +0800

    添加结算

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -53,6 +53,8 @@ class OrderController extends Controller
    //订单结算同步
    public function settlement(BbcOrderBill $order){
 
+
+
             $result=$this->dispatch(new sendCommission(getenv('K3_URL'),$order));
         if($result){
            //某个同步失败的标记
@@ -63,4 +65,26 @@ class OrderController extends Controller
             BbcOrderBill::update(['process'=>2,]);
        }
    }
+
+    public function settlementInsert()
+    {
+        //ob_no=date('Y-m',time()).$data['vid']
+        //ob_start_date=$data['finnshed_time'] 直接是时间戳
+        //ob_order_totals=$data['order_amount']//订单总金额
+        //ob_order_refound_offline=0//货到付款退款金额
+        //ob_shipping_totals=0；//运费
+        //ob_order_return_totals=$data['refund_amount']//退单金额
+        //ob_commis_totals=佣金金额//
+        //order_yongjin_totals=订单的商品分销佣金
+        //ob_commis_return_totals=$data['order_amount']-$data['refund_amount']//退还佣金
+        //ob_store_cost_totals=$data['red_money']//店铺促销活动费用
+        //ob_result_totals=应结金额
+        //
+        //$data['order_amount']+$data['pd_points']-order_yongjin_totals-$data['refund_amount']+ob_commis_return_totals-$data['red_money']
+        //
+        //180.00 (订单金额) +0.00(积分金额) - 0.00(货到付款金额) - 0.00 (佣金金额) - 0 (退单金额) + 0.00 (退还佣金) - 0.00 (促销费用)
+        //
+        //ob_pd_points=$data['pd_points']//积分金额
+   }
+
 }
